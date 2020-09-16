9029d68cca5325ec704510e5d209769d62e92215
SafalAryal <safalaryalofficial@gmail.com>
2020-09-16 20:45:55 +1000

    passport JWT

--- routes/promoRouter.js
+++ routes/promoRouter.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose')
 const Promotions = require('../models/promotions')
 const promoRouter = express.Router();
-
+const authenticate = require('../authenticate')
 promoRouter.use(bodyParser.json());
 
 promoRouter.route('/')
@@ -16,7 +16,7 @@ promoRouter.route('/')
             }, (err) => next(err))
             .catch((err) => next(err));
     })
-    .post((req, res, next) => {
+    .post(authenticate.verifyUser, (req, res, next) => {
         Promotions.create(req.body)
             .then((promo) => {
                 console.log('promo Created ', promo);
@@ -26,7 +26,7 @@ promoRouter.route('/')
             }, (err) => next(err))
             .catch((err) => next(err));
     })
-    .put((req, res, next) => {
+    .put(authenticate.verifyUser, (req, res, next) => {
         res.statusCode = 403;
         res.end('PUT operation not supported on /promos');
     })
@@ -50,11 +50,11 @@ promoRouter.route('/:promoId')
             }, (err) => next(err))
             .catch((err) => next(err));
     })
-    .post((req, res, next) => {
+    .post(authenticate.verifyUser, (req, res, next) => {
         res.statusCode = 403;
         res.end('POST operation not supported on /promo/' + req.params.promoId);
     })
-    .put((req, res, next) => {
+    .put(authenticate.verifyUser, (req, res, next) => {
         Promotions.findByIdAndUpdate(req.params.promoId, {
             $set: req.body
         }, { new: true })
@@ -65,7 +65,7 @@ promoRouter.route('/:promoId')
             }, (err) => next(err))
             .catch((err) => next(err));
     })
-    .delete((req, res, next) => {
+    .delete(authenticate.verifyUser, (req, res, next) => {
         Promotions.findByIdAndRemove(req.params.promoId)
             .then((resp) => {
                 res.statusCode = 200;
--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,8 @@ var express = require('express');
 const bodyParser = require('body-parser')
 var User = require('../models/user')
 var router = express.Router();
-var passport = require('passport')
+var passport = require('passport');
+var authenticate = require('../authenticate')
 
 router.use(bodyParser.json())
 
@@ -32,9 +33,11 @@ router.post('/signup', function (req, res, next) {
 })
 
 router.post('/login', passport.authenticate('local'), (req, res) => {
+  var token = authenticate.getToken({ _id: req.user._id });
+
   res.statusCode = 200
   res.setHeader('Content-Type', 'application/json')
-  res.json({ success: true, status: 'You are  Successfully logged in! ' })
+  res.json({ success: true, token: token, status: 'You are  Successfully logged in! ' })
 
 })
 router.get('/logout', (req, res) => {
